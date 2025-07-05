4a84342448c53e4a9c8de4a68ac5e0d121e48c52
trevor1107 <qnrudwns123@naver.com>
2025-07-05 23:37:41 +0900

    feat: Section 10. 66번 인터셉터 DTO 커스터마이즈

--- my-car-value/src/interceptors/serialize.interceptor.ts
+++ my-car-value/src/interceptors/serialize.interceptor.ts
@@ -7,13 +7,14 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           // 일반 값을 클래스로 변환 할 때 외부 속성이 값에서 제외되어야하는지 여부를 나타냅니다.
           excludeExtraneousValues: true,
         });
