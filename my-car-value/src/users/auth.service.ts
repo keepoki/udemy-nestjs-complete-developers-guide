c28ff3b61027f0ce296ea6c117ac5684cefe7623
trevor1107 <qnrudwns123@naver.com>
2025-07-06 22:04:13 +0900

    feat: Section 11 - 72. 가입하기 구현하기

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // 이메일이 사용 중인지 확인하십시오
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    // 사용자 비밀번호 해시
+
+    // 새 사용자를 만들고 저장하십시오
+
+    // return the user
+  }
+
+  signin() {
+
+  }
 }
