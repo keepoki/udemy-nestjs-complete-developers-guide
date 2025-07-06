ac03e762730a041aee6d68764d20398548e0cf37
trevor1107 <qnrudwns123@naver.com>
2025-07-07 00:06:32 +0900

    feat: Section 11 - 74. 해시와 솔트로 비밀번호 암호화하기

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -13,6 +17,14 @@ export class AuthService {
     }
 
     // 사용자 비밀번호 해시
+    // Salt 생성
+    const salt = randomBytes(8).toString('hex');
+
+    // 소금과 암호를 함께 해시로 변환
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // 해시 결과와 소금을 합치시오
+    const result = salt + '.' + hash.toString('hex');
 
     // 새 사용자를 만들고 저장하십시오
 
