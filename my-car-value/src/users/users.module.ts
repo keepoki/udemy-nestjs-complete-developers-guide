1d81efde4a3f9c1710fe8cd8fa12746226e7d714
trevor1107 <qnrudwns123@naver.com>
2025-07-24 23:22:11 +0900

    feat: Section 11 - 88. 전역적인 인터셉터

--- my-car-value/src/users/users.module.ts
+++ my-car-value/src/users/users.module.ts
@@ -5,10 +5,18 @@ import { UsersService } from './users.service';
 import { UserEntity } from './user.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
