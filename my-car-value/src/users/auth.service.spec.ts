5d5b9ee35d32eaff2115d4e99dd70d7eb7d3c98c
trevor1107 <qnrudwns123@naver.com>
2025-07-27 23:51:40 +0900

    feat: Section 12 - 91. 테스트 설정

---
+++ my-car-value/src/users/auth.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create an instance of auth service', async () => {
+  // 사용자 서비스의 가짜 사본을 만듭니다
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
+});
