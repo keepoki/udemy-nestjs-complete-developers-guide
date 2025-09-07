1899f176bdc1a39427bdb06638cecf1e9fc46e44
trevor1107 <qnrudwns123@naver.com>
2025-09-07 23:14:23 +0900

    feat: Section 14 - 123. 후속 테스트

--- my-car-value/test/auth.e2e-spec.ts
+++ my-car-value/test/auth.e2e-spec.ts
@@ -29,4 +29,25 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie') as string[];
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+      expect(body.email).toEqual(email);
+
+      // 실패 테스트
+      // expect(body.email).toEqual('sadf');
+  });
 });
