187a195db64599a0fcc26ca7b584910981f87c11
trevor1107 <qnrudwns123@naver.com>
2025-11-01 23:38:02 +0900

    feat: Section 15 - 135. 관계 설정하기

--- my-car-value/src/reports/reports.service.ts
+++ my-car-value/src/reports/reports.service.ts
@@ -3,13 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ReportEntity } from './reports.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { UserEntity } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(ReportEntity) private repo: Repository<ReportEntity>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: UserEntity) {
     const report = this.repo.create(reportDto);
+    report.user = user;
     return this.repo.save(report);
   }
 }
